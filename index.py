d97decbd40649cf4e254c39d8da85e8932fa7ceb
Evangeline Wong <112913399+EvWong00@users.noreply.github.com>
2023-05-17 19:53:46 +0000

    basic calculator

--- index.py
+++ index.py
@@ -2,6 +2,31 @@
 
 print("Select an operation to perform: ")
 print("1.Add")
-print("1.Subtract")
-print("1.Multiply")
-print("1.Divide")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+operation = input()
+
+if operation == "1":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The sum is " + str(int(num1) + int(num2)))
+
+elif operation == "2":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The difference is " + str(int(num1) - int(num2)))
+
+elif operation == "3":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The product is " + str(int(num1) * int(num2)))
+
+elif operation == "4":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The result is " + str(int(num1) / int(num2)))
+
+else:
+    print("Invalid Entry")
